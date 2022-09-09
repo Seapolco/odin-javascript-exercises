cf188068a1f6be446814499e691b72322edad9cf
Seapolco <seapolco@gmail.com>
2022-09-09 20:28:23 +0100

    Complete 08 - Calculater exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let numbers = array;
+  if(numbers.length === 0) {
+    return 0 ;
+  } 
+  let sum = numbers.reduce((acc,curr) => {
+    acc += curr;
+    return acc
+  })
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let sum = numbers.reduce((acc,curr) => {
+    acc *= curr;
+    return acc
+  })
 
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0 || number === 1) {
+    return 1
+  }
+  let numbersArray = [];
+
+  for(let i = number; i > 0; i--) {
+    numbersArray.push(i)
+  }
+
+  let sum = numbersArray.reduce((acc,curr) => {
+    return acc *= curr
+  })
+	return sum;
 };
 
 // Do not edit below this line
