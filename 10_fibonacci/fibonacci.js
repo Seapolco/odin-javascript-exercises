441feeb99fd95bd7ebc000cad4b6721e818a40be
Seapolco <seapolco@gmail.com>
2022-09-09 21:11:38 +0100

    Completed fibonacci task.
    Thought I had no chance with this one but I ended up figuring it out
    pretty quickly. No googling for techniques. Chuffed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if(number < 0) {
+    return 'OOPS'
+  }
 
+  let firstIndex = 0;
+  let secondIndex = 1;
+  let fibonacci = [firstIndex,secondIndex]
+  
+  for(let i = 0; i < +number; i++) {
+  fibonacci.push(fibonacci[firstIndex] + fibonacci[secondIndex])
+  firstIndex++
+  secondIndex++
+	}
+  return fibonacci[+number]
 };
 
 // Do not edit below this line
